2371ba569f75b9cccb943b9aa2dbb578c1bc8ab6
ViniciusDSDSouza <viniciusdossantos547@gmail.com>
2025-11-22 05:35:10 -0300

    fix: backend healthcheck in docker compose

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,8 +3,15 @@ import { DatabaseModule } from './database/database.module';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 import { WeatherModule } from './weather/weather.module';
+import { HealthModule } from './health/health.module';
 
 @Module({
-  imports: [DatabaseModule, AuthModule, UsersModule, WeatherModule],
+  imports: [
+    DatabaseModule,
+    AuthModule,
+    UsersModule,
+    WeatherModule,
+    HealthModule,
+  ],
 })
 export class AppModule {}
