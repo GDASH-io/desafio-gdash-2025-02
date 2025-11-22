37895254a303c082115c5b0c193b0298da0cb5fa
denzel <denzelwashingtonsantana@gmail.com>
2025-11-22 00:55:49 -0300

    Add users module with schema, encryption and admin seeding

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -4,6 +4,8 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { WeatherModule } from './weather/weather.module';
+import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
@@ -14,6 +16,8 @@ import { WeatherModule } from './weather/weather.module';
       'mongodb://admin:password123@localhost:27017/gdash?authSource=admin',
     ),
     WeatherModule,
+    UsersModule,
+    AuthModule,
   ],
   controllers: [AppController],
   providers: [AppService],
---
+++ backend/src/users/users.service.ts
@@ -0,0 +1,41 @@
+import { Injectable, Logger, OnModuleInit } from '@nestjs/common';
+
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { User, UserDocument } from './schemas/user.schema';
+
+import * as bcrypt from 'bcrypt';
+
+@Injectable()
+export class UsersService implements OnModuleInit {
+  private readonly logger = new Logger(UsersService.name);
+  constructor(@InjectModel(User.name) private userModel: Model<UserDocument>) {}
+
+  async onModuleInit() {
+    const adminEmail = 'admin@example.com';
+    const adminExists = await this.userModel.findOne({ email: adminEmail });
+
+    if (!adminExists) {
+      this.logger.log('🌱 Criando usuário Admin padrão...');
+      await this.create(adminEmail, '123456', 'Admin GDASH');
+    } else {
+      this.logger.log('✅ Usuário Admin já existe.');
+    }
+  }
+
+  async create(email: string, pass: string, name: string): Promise<User> {
+    const salt = await bcrypt.genSalt();
+    const hashedPassword = await bcrypt.hash(pass, salt);
+
+    const newUser = new this.userModel({
+      email,
+      password: hashedPassword,
+      name,
+    });
+    return newUser.save();
+  }
+
+  async findOne(email: string): Promise<UserDocument | null> {
+    return this.userModel.findOne({ email }).exec();
+  }
+}
