3dc0411c495188b8d7436f5c42fd2fd53f6cdc06
jp066 <jpedro030506@gmail.com>
2025-11-22 10:03:38 -0300

    feat: iniciando o servico Go Worker

--- services/go-worker/cmd/main.go
+++ services/go-worker/cmd/main.go
@@ -1,23 +1,16 @@
-package main
-
 import (
 	"context"
 	"time"
-
 	"github.com/jp066/desafio-gdash-2025-02/services/go-worker/internal/consumer"
 	"github.com/jp066/desafio-gdash-2025-02/services/go-worker/internal/logger"
 )
 
 func main() {
-	logger.Info("go-worker starting")
-
-	// graceful shutdown context
+	logger.Info("inicializando o worker")
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
 
-	// start a goroutine to listen for interrupt/term signals
 	go func() {
-		// block until context is cancelled in case parent wants to stop
 		<-ctx.Done()
 	}()
 
