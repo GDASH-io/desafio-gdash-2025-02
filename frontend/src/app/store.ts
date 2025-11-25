562f5e708939dd8557b9390fa1d344c0842e67e2
ViniciusDSDSouza <viniciusdossantos547@gmail.com>
2025-11-24 22:27:54 -0300

    feat: add redux toolkit query inital setup

---
+++ frontend/src/app/store.ts
@@ -0,0 +1,10 @@
+import { configureStore } from "@reduxjs/toolkit";
+import { useDispatch } from "react-redux";
+
+export const store = configureStore({
+  reducer: {},
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(),
+});
+
+export type AppDispatch = typeof store.dispatch;
+export const useAppDispatch = useDispatch.withTypes<AppDispatch>();
