8d3e7a2980602b48406ea2c0cf8143be6831005b
Gabriel Messias <gabrielmessias@outlook.com>
2025-11-26 18:46:21 -0300

    add metodo put na lista de usuarios

--- nestjs-api/src/users/users.controller.ts
+++ nestjs-api/src/users/users.controller.ts
@@ -1,7 +1,17 @@
-import { Body, Controller, Delete, Get, Param, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+  UseGuards,
+} from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { User, UserDocument } from './schemas/user.schema';
+import { AuthGuard } from '@nestjs/passport';
 
 @Controller('users')
 export class UsersController {
@@ -15,7 +25,7 @@ export class UsersController {
   }
 
   // Listar usuários (Protegido)
-  // @UseGuards(AuthGuard('jwt')) <--- Descomente depois para ativar segurança
+  @UseGuards(AuthGuard('jwt'))
   @Get()
   async findAll() {
     return this.userModel.find({}, { password: 0 }).exec(); // Não retorna a senha
@@ -25,5 +35,11 @@ export class UsersController {
   @Delete(':id')
   async delete(@Param('id') id: string) {
     return this.userModel.findByIdAndDelete(id);
+  }
+
+  @Put(':id')
+  async update(@Param('id') id: string, @Body() body: any) {
+    // Atualiza e retorna o novo documento ({ new: true })
+    return this.userModel.findByIdAndUpdate(id, body, { new: true });
   }
 }
